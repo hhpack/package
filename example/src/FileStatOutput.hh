6e72a5ce5a8856f4f0ee54ce60dbea617dc3cf8f
holyshared <holy.shared.design@gmail.com>
2016-03-31 10:13:41 +0900

    fix example

--- example/src/FileStatOutput.hh
+++ example/src/FileStatOutput.hh
@@ -12,7 +12,7 @@ final class FileStatOutput implements Middleware<SplFileInfo, void>
     public function receive(Stream<SplFileInfo> $stream) : void
     {
         foreach ($stream->items() as $stat) {
-            echo $stat->getPath(), ' - ', $stat->getSize(), 'bytes', PHP_EOL;
+            echo $stat->getPathname(), ' - ', $stat->getSize(), 'bytes', PHP_EOL;
         }
     }
 
